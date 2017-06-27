0f8376ff1b1eb306179e5c1be6577b3129d800f9
Hack Reactor Student <student@hackreactor.com>
2017-06-26 18:32:43 -0700

    "Tests passed for Each, IndexOf, and Filter"

--- src/underbar.js
+++ src/underbar.js
@@ -50,6 +50,15 @@
   // Note: _.each does not have a return value, but rather simply runs the
   // iterator function over each item in the input collection.
   _.each = function(collection, iterator) {
+    if (Array.isArray(collection)) {
+      for (var i = 0; i < collection.length; i++) {
+        iterator(collection[i], i, collection);
+      }
+    } else {
+      for (var key in collection) {
+        iterator(collection[key], key, collection);
+      }
+    }
   };
 
   // Returns the index at which value can be found in the array, or -1 if value
@@ -71,6 +80,13 @@
 
   // Return all elements of an array that pass a truth test.
   _.filter = function(collection, test) {
+    var returnArr = [];
+    _.each(collection, function(value) {
+      if (test(value)) {
+        returnArr.push(value);
+      }
+    });
+    return returnArr;
   };
 
   // Return all elements of an array that don't pass a truth test.
